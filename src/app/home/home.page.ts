96301d721b425de7715500d63e6581e87eccbbbf
nikhil tk <nikhiltk02@gmail.com>
2022-08-07 05:07:45 +0530

    Little bit of change to test ionic First App

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  text = 'Ready to create an app?';
+  onChangeText() {
+    this.text = 'Ohh! I am born Ready!';
+  }
 }
